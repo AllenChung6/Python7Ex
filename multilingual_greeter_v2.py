34ac452d126a0ab09b0c4a33f75197ed2f3e078b
Allen C <allenc@Zipcoders-MacBook-Pro.local>
2022-03-20 22:40:59 -0400

    Finished Exercise 2.

--- multilingual_greeter_v2.py
+++ multilingual_greeter_v2.py
@@ -24,7 +24,10 @@ def print_language_options(lang_options: Dict[int, str]) -> None:
     for x in lang_options:
         print(f'{x}: {lang_options[x]}')
 
-    return None
+
+def print_greeting_options(greet_options: Dict[int, str]) -> None:
+    for x in greet_options:
+        print(f'{x}: {greet_options[x]}')
 
 
 def language_input() -> int:
@@ -90,13 +93,17 @@ def greet(name: str, greetings_options: Dict[int, str], lang_choice: int) -> Non
 
 
 def select_mode():
-    mode = (int(input('Please enter 1 for admin mode or 2 for user mode:\n')))
-    if mode == 1:
-        admin_mode()
-    elif mode == 2:
-        user_mode()
-    else:
-        print('Invalid input')
+    while True:
+        mode = (int(input('Please enter 1 for admin mode or 2 for user mode or 3 to exit:\n')))
+        if mode == 1:
+            admin_mode()
+        elif mode == 2:
+            user_mode()
+        elif mode == 3:
+            break
+        else:
+            print('Invalid input')
+            break
 
 
 def user_mode():
@@ -117,15 +124,21 @@ def admin_mode():
     print('You are in admin mode')
     while True:
         try:
-            admin_option = int(input('Please select 1 to add additional languages or select 2 for update greetings for existing languages. \
-Please select 3 to cancel adding options.\n'))
+            admin_option = int(input('Please select 1 to add additional languages or select 2 for adding What is your name? in selected language. \
+ Please type 3 to add a new greeting in your language. Please select 0 to cancel adding options.\n'))
             if admin_option == 1:
-                add_lang = input('Please type in a new language to add. Please select 3 to cancel adding options.\n')
+                add_lang = input('Please type in a new language to add. Please select 0 to cancel adding options.\n')
                 end_dict = len(lang_dict) + 1
                 lang_dict[end_dict] = add_lang
             elif admin_option == 2:
-                print('You want to edit greetings')
+                add_name = input('Please type (What is your name?) in selected language. Please select 0 to cancel adding options.\n')
+                end_name = len(name_prompt_dict) + 1
+                name_prompt_dict[end_name] = add_name
             elif admin_option == 3:
+                add_greeting = input('Please type in a new greeting to add. Please select 3 to cancel adding options.\n')
+                end_greeting = len(greetings_dict) + 1
+                greetings_dict[end_greeting] = add_greeting
+            elif admin_option == 0:
                 print('Canceled adding languages.')
                 break
             else:
@@ -133,7 +146,9 @@ Please select 3 to cancel adding options.\n'))
         except:
             print('Invalid selection!')
 
-    print_language_options(lang_dict)
+    print(f'New Language Menu: {lang_dict}')
+    print(f'New What is your Name {name_prompt_dict}? Menu')
+    print(f'New Greetings Menu {greetings_dict}')
 
 
 select_mode()
